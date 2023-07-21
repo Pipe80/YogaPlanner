f194d96d497ccde6cc92ad2dcd6b4896f0a8f39f
Pipe80 <pirjomure@hotmail.com>
2023-07-21 14:46:58 +0300

    update settings page

--- react-yogaplanner/src/components/Settings.js
+++ react-yogaplanner/src/components/Settings.js
@@ -1,33 +1,50 @@
 import React from "react";
-import "../styles/SettingStyle.css";
-import Logo from "../assets/Logo.png";
+import "../styles/FrontPageStyles.css";
+import Nav from "./SettingsNav";
+import { motion } from "framer-motion";
+import { titleAnim } from "../styles/Animations";
+import { FaGlobe, FaRegEnvelope } from 'react-icons/fa';
 
 const Settings = () => {
-  return (
-    <div className="settings-page">
-      <div className="settings-header">
-        <img src={Logo} className="logo" alt="Logo" />
-        <h2>Yogaplanner</h2>
-        <div className="nav-buttons">
-          <button className="settings-button">Settings</button>
-          <button className="logout-button">Log Out</button>
-        </div>
-      </div>
-      <div className="settings-content">
-        <div className="left-sidebar">
-          <div className="left-sidebar-content">
-            <button className="back-button">Back</button>
-            <button className="save-button">Save</button>
-          </div>
-        </div>
-        <div className="settings-center">
-          <div className="language-menu"></div>
-          <div className="change-password-menu"></div>
-          <div className="change-email-menu"></div>
-        </div>
-      </div>
-    </div>
-  );
-};
+    return (
+        <div className="settings">
+            <Nav/>
+            <div className="settings-container">
 
+            <div className="left-sidebar">
+                    <div className="left-sidebar-content">
+                        <button className="back-button">Back</button>
+                        <button className="save-button">Save</button>
+                    </div>
+                </div>
+
+                <motion.div 
+                    variants={titleAnim} 
+                    initial="hidden"
+                    animate="show"
+                    className="frontpage-text"
+                >
+                    <div className="frontpage-title">
+
+                        <h3>FI/EN</h3>
+                        <h2><FaRegEnvelope style={{color: 'green'}}/></h2>
+                        <h3 style={{color: 'green'}}>change your email</h3>
+                        <h2><FaGlobe style={{color: 'blue'}}/></h2>
+                        <h3 style={{color: 'blue'}}>change your password</h3>
+                        <motion.h3 variants={titleAnim}>
+                           Change your password
+                        </motion.h3>
+                    </div>
+                    <motion.div 
+                        variants={titleAnim}
+                        className="ctas"
+                    >
+                    </motion.div>
+                </motion.div>
+                
+            </div>
+        </div>
+    );
+}
+ 
 export default Settings;
---
+++ react-yogaplanner/src/components/SettingsNav.js
@@ -0,0 +1,67 @@
+import React, { useState, useEffect } from "react";
+import "../styles/NavStyles.css";
+import { motion } from "framer-motion";
+import { navAnim } from "../styles/Animations";
+import Logo from "../assets/Logo.png";
+
+const SettingsNav = () => {
+  const [active, setActive] = useState("nav-links");
+  const [toggleIcon, setToggleIcon] = useState("burger-menu");
+
+  useEffect(() => {
+    setActive("nav-links"); // Start with nav-links closed
+    setToggleIcon("burger-menu"); // Start with burger-menu icon
+  }, []);
+
+  const navToggle = () => {
+    if (active === "nav-links") {
+      setActive("nav-links"); // Close the mobile menu first
+
+      // Delay applying "nav-active" class to mobile menu
+      setTimeout(() => {
+        setActive("nav-links nav-active");
+      }, 500); // Adjust the delay time as needed
+    } else {
+      setActive("nav-links"); // Close the mobile menu
+    }
+
+    // Toggle Animation with Delay
+    if (toggleIcon === "burger-menu") {
+      setToggleIcon("burger-menu toggle");
+    } else {
+      setTimeout(() => {
+        setToggleIcon("burger-menu");
+      }, 500); // Adjust the delay time as needed
+    }
+  };
+
+  return (
+    <motion.nav variants={navAnim} initial="hidden" animate="show" className="navbar">
+      <div className="logo">
+        
+        <img src={Logo} className="logo" alt="Logo" />
+        
+        <h2>YogaPlanner</h2>
+      </div>
+      <ul onClick={navToggle} className={active}>
+      <li>
+          </li>
+        <div className="buttons">
+          <li>
+            <button className="sign-button" href="#">Settings</button>
+          </li>
+          <li>
+            <button className="login-button" href="#">Log Out</button>
+          </li>
+        </div>
+      </ul>
+      <div onClick={navToggle} className={toggleIcon}>
+        <div className="line1"></div>
+        <div className="line2"></div>
+        <div className="line3"></div>
+      </div>
+    </motion.nav>
+  );
+};
+
+export default SettingsNav;
