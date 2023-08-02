f14387dd912ac92e60137f6b38b9b0c4d688ed75
Bella <116814686+devBellaR@users.noreply.github.com>
2023-08-02 13:37:14 +0300

    Removing unnecessary lines

--- react-yogaplanner/src/components/SignUp.js
+++ react-yogaplanner/src/components/SignUp.js
@@ -1,14 +1,12 @@
 import React from "react";
 import "../styles/LoginStyles.css";
-/* import Nav from "./Nav"; */
 import { motion } from "framer-motion";
 import { titleAnim } from "../styles/Animations";
 import Logo from "../assets/Logo_test.png";
 
-const SignIn = () => {
+const SignUn = () => {
   return (
     <div className="login-page">
-      {/* <Nav /> */}
       <motion.div 
         variants={titleAnim}
         initial="hidden"
@@ -20,10 +18,8 @@ const SignIn = () => {
               <img src={Logo} alt="YogaPlanner-logo" className="logo-login" />
             </div>
           </div>
-            {/* <label htmlFor="email">Email</label> */}
             <input type="text" id="name" placeholder="Name" className="input-field" />
             <input type="text" id="email" placeholder="Email" className="input-field" />
-            {/* <label htmlFor="password">Password</label> */}
             <input type="password" id="password" placeholder="Password" className="input-field" />
             <input type="passwordAgain" id="passwordAgain" placeholder="Password again" className="input-field" />
             <button className="login-btn">Sign Up</button>
@@ -32,4 +28,4 @@ const SignIn = () => {
   );
 };
 
-export default SignIn;
+export default SignUn;
